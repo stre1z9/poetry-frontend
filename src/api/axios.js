2f4c3f001a360de632ee4dd616b40fd383bb8502
stre1z9 <114341458+stre1z9@users.noreply.github.com>
2026-07-24 04:18:50 +0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,11 +5,12 @@ const api = axios.create({
 
     baseURL: import.meta.env.VITE_API_URL/api
 
-    console.log(
+    
+});
+console.log(
         "API URL:",
         import.meta.env.VITE_API_URL
     );
-});
 
 
 api.interceptors.request.use(
